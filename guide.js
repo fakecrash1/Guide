d1b71ce6571730de020377f3c8c348732991a7ce
fakecrash1 <fakecrash@gmail.com>
2023-06-03 19:55:33 +0200

    Co-authored-by: szoszo23 <szoszo23@users.noreply.github.com>

--- guide.js
+++ guide.js
@@ -1,18 +1,27 @@
-// While loop
+// Creating variables
 let myArray = 25
+const myArray2 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15] // can not change
+
+console.log(myArray)
+console.log(myArray2)
+
+// While loop - 
 let i = 0;
 while (i < myArray){
-    console.log("My array: ",i)
+    console.log("While loop: ",i)
     i++
 }
 
 // Do while loop
 let count = 0;
 do {
-    console.log("Do while : ", count);
+    console.log("Do... : ", count);
     count++;
     }while (count < 5);
-        console.log("Do while stopped!", count);
+        console.log("while...", count);
+
+
+
 
 // For loop < 10
 for (let j = 0; j < 10; j++){
@@ -31,10 +40,7 @@ for (let j = 0; j < 15; j+=3){
 
 
 
-
-
-
-
+/*
 // For in
 
 const salaries= {
@@ -50,5 +56,6 @@ for ( let i in salaries) {
     let salary = "$" + salaries[i];
 
     // display the values
-    console.log(`${i} : ${salary}`);
+    console.log("Salary: ",salary);
 }
+*/
